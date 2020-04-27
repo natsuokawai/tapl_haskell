b861db08120fa13be66b62a9c2211018489a9587
natsuokw <kawai.natsuo@gmail.com>
2020-04-27 21:01:47 +0900

    add big-step evaluator

--- chapter4/arithmetic_expressions.hs
+++ chapter4/arithmetic_expressions.hs
@@ -21,7 +21,7 @@ isVal TmFalse              = True
 isVal t1 | isNumericVal t1 = True
 isVal _                    = False
 
--- the single-step evaluator 
+-- single-step evaluator 
 eval1 :: Term -> Maybe Term
 eval1 (TmIf TmTrue t2 t3)     = Just t2
 eval1 (TmIf TmFalse t2 t3)    = Just t3
@@ -49,3 +49,20 @@ eval :: Term -> Maybe Term
 eval t = case eval1 t of
            Just t' -> eval1 t'
            Nothing -> Just t
+
+-- big-step evaluator
+eval' :: Term -> Maybe Term
+eval' v | isVal v     = Just v
+eval' (TmIf t1 t2 t3) = case eval' t1 of
+                          Just TmTrue                   -> eval' t2
+                          Just TmFalse                  -> eval' t3
+eval' (TmSucc t1)     = case eval' t1 of
+                          Just nv1 | isVal nv1          -> Just (TmSucc nv1)
+eval' (TmPred t1)     = case eval' t1 of
+                          Just TmZero                   -> Just TmZero
+                          Just (TmSucc nv1) | isVal nv1 -> Just nv1
+eval' (TmIsZero t1)   = case eval' t1 of
+                          Just TmZero                   -> Just TmTrue
+                          Just (TmSucc nv1) | isVal nv1 -> Just TmFalse
+eval' _               = Nothing
+
